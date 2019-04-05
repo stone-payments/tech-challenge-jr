5477ba68901261de1d8995bdf559a3f5e0d15ebd
Christophe Nonato <chr.nonato@protonmail.com>
2019-04-05 01:09:01 -0300

    adicionado HTTPoison nas dependencias da mix.exs, e usado na extracao de dados da exchange_rates API

--- lib/financial_system.ex
+++ lib/financial_system.ex
@@ -1,6 +1,11 @@
 defmodule FinancialSystem do
-  @moduledoc """
-  Documentation for FinancialSystem.
-  """
+
+  # Usando dados da exchange rates API através do HTTPoison (https://exchangeratesapi.io/)
+  url = "https://api.exchangeratesapi.io/latest"
+  HTTPoison.start()
+  response = HTTPoison.get!(url)
+  {:ok, response} = {:ok, Poison.decode!(response.body)}
+  @attr response
+
 
 end
